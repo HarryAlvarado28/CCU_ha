f64d71da6802ba3649f4a2ee48fbe2f5adaf012e
Harry Alvarado <harry2alvarado8@gmail.com>
2018-06-29 17:18:47 -0500

    En desarrollo del script de linux.

--- scriptUnix.sh
+++ scriptUnix.sh
@@ -1,1 +1,24 @@
 #!/usr/bin/env bash
+
+read -p "Ruta origen: " ori
+read -p "Ruta destino: " dts
+read -p "Tiempo: " tt
+
+echo "Ruta de origen: $ori"
+echo "Ruta de destino: $dts"
+echo "tiempo: $tt"
+
+cont=1
+touch backup.log
+echo "-$ori" >> backup.log
+echo "-$dts" >> backup.log
+echo "-$tt" >> backup.log
+while true; do
+    cont=$[$cont+1]
+    ## cp -R "" ""
+    date >> backup.log
+    echo "Copia:$cont " >> backup.log
+    sleep $tt
+    date;
+    echo "  ¡¡Copia realizada!!! backup.log : $cont"
+done
