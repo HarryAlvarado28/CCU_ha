83262e7fd2b434758eae77e9a0bcc314dcd2b7f9
Harry Alvarado <harry2alvarado8@gmail.com>
2018-07-11 09:57:20 -0500

    Se prepara entorno de trabajo, para dar inicio al ejercicio propuesto.

---
+++ r15/F5.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author harry
+ *
+ */
+public class F5 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String var1 = "Esto es una variable string";
+		System.out.println("¿Qué es var1?, "+var1);
+	}
+
+}
